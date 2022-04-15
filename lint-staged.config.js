81018380d24cc6ffc926461d0f46915f3e4b61b3
Kevin Aminzadeh <kevin@convertdigital.com.au>
2022-04-15 19:27:49 +0930

    chore: fix stylelint lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,7 +5,7 @@ const customConfig = {
     'npm run prettier:fix',
     'npm run test:unit:staged',
   ],
-  'src/**/*.css?(x)': [() => 'npm run lint:styles:staged'],
+  'src/**/*.css?(x)': ['npm run lint:styles:staged', 'npm run prettier:fix'],
 }
 
 module.exports = customConfig
