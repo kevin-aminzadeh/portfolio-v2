ae5711d04d94e28ae8d692c108954b698dcedb59
Kevin Aminzadeh <kevin@convertdigital.com.au>
2022-04-15 19:20:32 +0930

    chore: add stylelint script to lint-staged tasks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,6 +5,7 @@ const customConfig = {
     'npm run prettier:fix',
     'npm run test:unit:staged',
   ],
+  'src/**/*.css': [() => 'npm run lint:styles:staged'],
 }
 
 module.exports = customConfig
