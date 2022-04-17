fbc245f7aa890ca4fa9d71d5ce62541dac20512f
Kevin Aminzadeh <kevin@convertdigital.com.au>
2022-04-17 18:49:52 +0930

    chore: fix Tailwind config components path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*/{js,ts,jsx,tsx}',
+    './src/common/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     fontFamily: {
