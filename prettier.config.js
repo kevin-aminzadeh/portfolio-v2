c91ab828762def72e3bde05c93aa8c872fedad4b
Kevin Aminzadeh <kevin@convertdigital.com.au>
2022-04-17 18:51:37 +0930

    chore: install and configure auto-sort/order plugins for ESLint and Prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  endOfLine: 'lf',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  useTabs: false,
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
